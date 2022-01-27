ac27753a8ba4df6d608d2652c7da9cda7ca4256a
George47 <george.siqi.wang@gmail.com>
2022-01-27 15:48:18 -0500

    initiate todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,8 +4,43 @@ import React from 'react';
 import '../assets/css/TodoList.css';
 
 class TodoList extends React.Component {
+    constructor() {
+        super();
+
+        // initiate to-do list with sample placeholders within state
+        this.state = {
+            todos: [
+                {
+                    description: "Walk the dog",
+                }, {
+                    description: "Clean the kitchen",
+                }
+            ]
+        }
+    }
+
     render() { 
-        return <div></div>;
+        return (
+            <div className="container">
+                {/* app header */}
+                <div className="app-header">
+                    test
+                </div>
+                {/* to-do list content */}
+                <h3>List of To-dos</h3>
+                <ul className="todo-list">
+                    {
+                        this.state.todos.map((v,k) => {
+                            return (
+                                <div>
+                                    {v.description}
+                                </div>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        );
     }
 }
  
